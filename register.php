c4d4ba52397d0172f2715f6aef0365209980210c
Bryan Eriksson <bryaneriksson@gmail.com>
2023-12-18 19:59:45 -0800

    More code

--- register.php
+++ register.php
@@ -1,3 +1,67 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "social");
+
+if(mysqli_connect_errno()) {
+    echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+// Declaring variables to prevent errors
+$fname = ""; // First name
+$lname = ""; // Last name
+$em = ""; // email
+$em2 = ""; // email 2
+$password = "";
+$password2 = "";
+$date = "";
+$error_array = "";
+
+if(isset($_POST['register_button'])) {
+
+    // Registration form values
+
+    // First name
+    $fname = strip_tags($_POST['reg_fname']);
+    $fname = str_replace(' ', '', $fname);
+    $fname = ucfirst(strtolower($fname));
+
+    // Last name
+    $lname = strip_tags($_POST['reg_lname']);
+    $lname = str_replace(' ', '', $lname);
+    $lname = ucfirst(strtolower($lname));
+
+    // email
+    $em = strip_tags($_POST['reg_email']);
+    $em = str_replace(' ', '', $em);
+    $em = ucfirst(strtolower($em));
+
+    // email 2
+    $em2 = strip_tags($_POST['reg_email2']);
+    $em2 = str_replace(' ', '', $em2);
+    $em2 = ucfirst(strtolower($em2));
+
+    // Password
+    $password = strip_tags($_POST['reg_password']);
+    $password2 = strip_tags($_POST['reg_password2']);
+
+    $date = date("Y-m-d"); // Current date
+
+    if($em == $em2) {
+        // Check if email is in valid format
+        if(filter_var($em, FILTER_VALIDATE_EMAIL)) {
+            
+            $em = filter_var($em, FILTER_VALIDATE_EMAIL);
+        }
+        else {
+            echo "Invalid format";
+        }
+    }
+    else {
+        echo "Emails don't match";
+    }
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
